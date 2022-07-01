65ebe28a50a829ca449b3a296ada8280aaefbe18
Julien Loir <mini.jul.jl@gmail.com>
2022-07-01 12:21:05 +0200

    Android webview

--- Android.mk
+++ Android.mk
@@ -82,6 +82,7 @@ LOCAL_SRC_FILES := \
 	src/unix/linux/android/jnih.c \
 	src/unix/linux/android/system.c \
 	src/unix/linux/android/tls.c \
+	src/unix/linux/android/webview.c \
 	src/unix/linux/android/gfx/gl-ctx.c
 
 include $(BUILD_STATIC_LIBRARY)
